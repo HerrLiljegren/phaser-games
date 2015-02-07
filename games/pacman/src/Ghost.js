4301c5ce4a53a5f655632f8868cc3a19ddcf8273
herrliljegren <jesper.liljegren@gmail.com>
2015-02-07 10:33:31 +0000

    Refactored Ghost.js a bit, added simple chase AI when the Ghost sees the player (radius collision only), named all ghosts and added ability to use different behaviours

--- games/pacman/src/Ghost.js
+++ games/pacman/src/Ghost.js
@@ -16,34 +16,39 @@ Pacman.Ghost = function(game, player, index) {
             this._addRedAnimations();
             this.homePosition = new Phaser.Point(6 * 32, 1 * 32);
             this.startPosition = new Phaser.Point(32 * 9, 32 * 11);
+            this.name = "Red";
             break;
 
         case 1:
             this._addYellowAnimations();
             this.homePosition = new Phaser.Point(21 * 32, 1 * 32);
             this.startPosition = new Phaser.Point(32 * 14, 32 * 13);
+            this.name = "Yellow";
             break;
 
         case 2:
             this._addPinkAnimations();
             this.homePosition = new Phaser.Point(8 * 32, 29 * 32);
             this.startPosition = new Phaser.Point(32 * 13, 32 * 14);
+            this.name = "Pinky";
             break;
 
         case 3:
             this._addBlueAnimations();
             this.homePosition = new Phaser.Point(19 * 32, 29 * 32);
             this.startPosition = new Phaser.Point(32 * 14, 32 * 14);
+            this.name = "Blue";
             break;
     }
-    
+
     this.sprite.reset(this.startPosition.x, this.startPosition.y);
     this.tweenDirection = new Phaser.Point(this.startPosition.x, this.startPosition.y);
-    
-    
+    this.targetPosition = this.homePosition;
 
 
-    this.tileSpeed = 250;
+
+
+    
     this.direction = Phaser.RIGHT;
     this.tweenDirection;
     this.moving = false;
@@ -53,87 +58,42 @@ Pacman.Ghost = function(game, player, index) {
 
 Pacman.Ghost.prototype.update = function() {
 
+    //this.targetPosition = this.player.sprite.position;
     if (!this.moving) {
 
-        //var tweenDirection = this._getTweenDirection(this.sprite, this.direction)
         this.game.add.tween(this.sprite)
-            .to(this.tweenDirection, this.tileSpeed, Phaser.Easing.Linear.None, true)
-            .onComplete.add(function() {
-                this.moving = false;
+            .to(this.tweenDirection, Pacman.Ghost.Behaviour[this.name].maxSpeed, Phaser.Easing.Linear.None, true)
+            .onComplete.add(this._calculateMovement, this);
 
 
-                var tile = new Phaser.Point(this.sprite.x / 32, this.sprite.y / 32);
-                var surroundingTiles = Pacman.Helpers.Tilemap.getSurroundingTiles(this.tilemap.map, tile.x, tile.y, this.tilemap.level);
+        if (this.player.isSuper) {
+            this.sprite.animations.play('weak');
+        }
+        else {
+            switch (this.direction) {
+                case Phaser.UP:
+                    this.sprite.animations.play('up');
+                    break;
+                case Phaser.DOWN:
+                    this.sprite.animations.play('down');
+                    break;
+                case Phaser.LEFT:
+                    this.sprite.animations.play('left');
+                    break;
+                case Phaser.RIGHT:
+                    this.sprite.animations.play('right');
+                    break;
+            }
+        }
 
-                var validDirections = {
-                    canMoveUp: this.direction !== Phaser.DOWN && this.tilemap.passableTiles.indexOf(surroundingTiles.up.index) > -1,
-                    canMoveDown: this.direction !== Phaser.UP && this.tilemap.passableTiles.indexOf(surroundingTiles.down.index) > -1,
-                    canMoveLeft: this.direction !== Phaser.RIGHT && this.tilemap.passableTiles.indexOf(surroundingTiles.left.index) > -1,
-                    canMoveRight: this.direction !== Phaser.LEFT && this.tilemap.passableTiles.indexOf(surroundingTiles.right.index) > -1
-                }
-
-                var validTiles = []
-                if (validDirections.canMoveUp) {
-                    validTiles.push({
-                        tile: surroundingTiles.up,
-                        direction: Phaser.UP
-                    });
-                }
-                if (validDirections.canMoveDown) {
-                    validTiles.push({
-                        tile: surroundingTiles.down,
-                        direction: Phaser.DOWN
-                    });
-                }
-                if (validDirections.canMoveLeft) {
-                    validTiles.push({
-                        tile: surroundingTiles.left,
-                        direction: Phaser.LEFT
-                    });
-                }
-                if (validDirections.canMoveRight) {
-                    validTiles.push({
-                        tile: surroundingTiles.right,
-                        direction: Phaser.RIGHT
-                    });
-                }
-
-
-
-                if (validTiles.length > 0) {
-                    this.tweenDirection = this._calculateDirection(validTiles, this.player);
-                }
-                else {
-                    this.tweenDirection = {
-                        x: this.sprite.x,
-                        y: this.sprite.y - 32
-                    };
-                    this.direction = Phaser.UP;
-                }
-
-                if (this.player.isSuper) {
-                    this.sprite.animations.play('weak');
-                }
-                else {
-                    switch (this.direction) {
-                        case Phaser.UP:
-                            this.sprite.animations.play('up');
-                            break;
-                        case Phaser.DOWN:
-                            this.sprite.animations.play('down');
-                            break;
-                        case Phaser.LEFT:
-                            this.sprite.animations.play('left');
-                            break;
-                        case Phaser.RIGHT:
-                            this.sprite.animations.play('right');
-                            break;
-                    }
-                }
-
-
-            }, this);
-
+        var canISeeYou = this.isPlayerVisible(this.player.sprite);
+        if (canISeeYou) {
+            console.log(this.name, "sees player");
+            this.targetPosition = this.player.sprite.position;
+        }
+        else {
+            this.targetPosition = this.homePosition;
+        }
         this.moving = true;
     }
 };
@@ -142,15 +102,115 @@ Pacman.Ghost.prototype.render = function() {
     //this.game.debug.body(this.sprite);
 
     this.game.debug.spriteInfo(this.sprite, 32 * 28, 32);
+
+
+    var c = new Phaser.Circle(this.sprite.body.center.x, this.sprite.body.center.y, Pacman.Ghost.Behaviour[this.name].viewDistance);
+    this.game.debug.geom(c);
 };
 
-Pacman.Ghost.prototype._calculateDirection = function(validTiles, player) {
+Pacman.Ghost.prototype.isPlayerVisible = function(player) {
+    var distance = Phaser.Math.distance(this.sprite.body.center.x, this.sprite.body.center.y, player.body.center.x, player.body.center.y);
+    return distance < 300;
+};
+
+Pacman.Ghost.prototype._calculateMovement = function() {
+    this.moving = false;
+
+
+    var tile = new Phaser.Point(this.sprite.x / 32, this.sprite.y / 32);
+    var surroundingTiles = Pacman.Helpers.Tilemap.getSurroundingTiles(this.tilemap.level, tile.x, tile.y);
+    var validDirections = Pacman.Helpers.Tilemap.getValidDirections(this.direction, this.tilemap.passableTiles, surroundingTiles);
+    var validTiles = Pacman.Helpers.Tilemap.getValidTilesToMoveTo(validDirections, surroundingTiles);
+
+    if (validTiles) {
+        this.tweenDirection = Pacman.Ghost.Behaviour[this.name].calculateDirection(validTiles, this.targetPosition);
+        this.direction = this.tweenDirection.direction;
+    }
+    else if (this.sprite.x === 0 && this.direction === Phaser.LEFT) {
+        this.tweenDirection = {
+            x: this.sprite.x - 32,
+            y: this.sprite.y
+        };
+    }
+    else if (this.sprite.x < 0) {
+        this.sprite.x = 864 + 32;
+
+        this.tweenDirection = {
+            x: this.sprite.x = 864,
+            y: this.sprite.y
+        };
+        this.direction = Phaser.LEFT;
+    }
+    else if (this.sprite.x === 864 && this.direction === Phaser.RIGHT) {
+        this.tweenDirection = {
+            x: this.sprite.x + 32,
+            y: this.sprite.y
+        };
+    }
+    else if (this.sprite.x > 864) {
+        this.sprite.x = -32;
+        this.tweenDirection = {
+            x: this.sprite.x = 0,
+            y: this.sprite.y
+        };
+        this.direction = Phaser.RIGHT;
+    }
+    else {
+        this.tweenDirection = {
+            x: this.sprite.x,
+            y: this.sprite.y - 32
+        };
+        this.direction = Phaser.UP;
+    }
+};
+
+Pacman.Ghost.Behaviour = {
+    "Red": {
+        maxSpeed: 250,
+        viewDistance: 600,
+        calculateDirection: _calculateDirection
+    },
+    "Blue": {
+        maxSpeed: 250,
+        viewDistance: 300,
+        calculateDirection: function(validTiles, target) {
+            var dist = 0; // egentligen: widht + height
+            var idx = -1;
+            for (var i = 0, l = validTiles.length; i < l; i++) {
+                var newDist = Math.abs(validTiles[i].tile.worldX - target.x) + Math.abs(validTiles[i].tile.worldY - target.y);
+
+                if (newDist < dist)
+                    continue;
+                dist = newDist;
+                idx = i;
+            }
+
+
+            return {
+                direction: validTiles[idx].direction,
+                x: validTiles[idx].tile.worldX,
+                y: validTiles[idx].tile.worldY
+            }
+        }
+    },
+    "Yellow": {
+        maxSpeed: 250,
+        viewDistance: 300,
+        calculateDirection: _calculateDirection
+    },
+    "Pinky": {
+        maxSpeed: 250,
+        viewDistance: 300,
+        calculateDirection: _calculateDirection
+    }
+}
+
+function _calculateDirection(validTiles, target) {
 
     //var target = player.sprite.position;
     //var target = new Phaser.Point(23*30, 1*32);
-    var target = this.homePosition;
 
-    var dist = 99999; // egentligen: widht + height
+    var dist = 999990; // egentligen: widht + height
     var idx = -1;
     for (var i = 0, l = validTiles.length; i < l; i++) {
         var newDist = Math.abs(validTiles[i].tile.worldX - target.x) + Math.abs(validTiles[i].tile.worldY - target.y);
@@ -161,8 +221,9 @@ Pacman.Ghost.prototype._calculateDirection = function(validTiles, player) {
         idx = i;
     }
 
-    this.direction = validTiles[idx].direction;
+
     return {
+        direction: validTiles[idx].direction,
         x: validTiles[idx].tile.worldX,
         y: validTiles[idx].tile.worldY
     }
@@ -172,56 +233,7 @@ Pacman.Ghost.prototype._calculateDirection = function(validTiles, player) {
 
     // ogogogog fastre!!!1!! Vad tror du om att istället för att skicka in validDirections skicka in dom tile-indexarna han kan gå till?
     // Sen kan du kolla alla tiles och se vilken som är närmast spelaren. :)
-    debugger;
 };
-
-Pacman.Ghost.prototype._getTweenDirection = function(sprite, direction) {
-    var x, y;
-
-    switch (direction) {
-        case Phaser.UP:
-            x = sprite.x;
-            y = sprite.y - 32;
-            break;
-
-        case Phaser.DOWN:
-            x = sprite.x;
-            y = sprite.y + 32;
-            break;
-
-        case Phaser.LEFT:
-            x = sprite.x - 32;
-            y = sprite.y;
-            break;
-
-        case Phaser.RIGHT:
-            x = sprite.x + 32;
-            y = sprite.y;
-            break;
-    }
-
-    return {
-        x: x,
-        y: y
-    }
-}
-
-Pacman.Ghost.prototype.wallCollision = function(ghost, wall) {
-    return;
-
-    if (ghost.body.touching.left) {
-        // ghost is touching the wall to its left
-    }
-    if (ghost.body.touching.right) {
-        // ghost is touching the wall to its right
-    }
-    if (ghost.body.touching.up) {
-        ghost.body.velocity.set(this.maxSpeed, 0);
-    }
-    if (ghost.body.touching.down) {
-        // ghost is touching the wall below
-    }
-}
 
 
 Pacman.Ghost.prototype._addRedAnimations = function() {
@@ -229,33 +241,29 @@ Pacman.Ghost.prototype._addRedAnimations = function() {
     this.sprite.animations.add('down', [14, 15], 4, true);
     this.sprite.animations.add('left', [28, 29], 4, true);
     this.sprite.animations.add('up', [42, 43], 4, true);
-}
+};
 
 Pacman.Ghost.prototype._addYellowAnimations = function() {
     this.sprite.animations.add('right', [2, 3], 4, true);
     this.sprite.animations.add('down', [16, 17], 4, true);
     this.sprite.animations.add('left', [30, 31], 4, true);
     this.sprite.animations.add('up', [44, 45], 4, true);
-}
+};
 
 Pacman.Ghost.prototype._addPinkAnimations = function() {
     this.sprite.animations.add('right', [4, 5], 4, true);
     this.sprite.animations.add('down', [18, 19], 4, true);
     this.sprite.animations.add('left', [32, 33], 4, true);
     this.sprite.animations.add('up', [46, 47], 4, true);
-}
+};
 
 Pacman.Ghost.prototype._addBlueAnimations = function() {
     this.sprite.animations.add('right', [6, 7], 4, true);
     this.sprite.animations.add('down', [20, 21], 4, true);
     this.sprite.animations.add('left', [34, 35], 4, true);
     this.sprite.animations.add('up', [48, 49], 4, true);
-}
+};
 
 Pacman.Ghost.prototype.setTilemap = function(tilemap) {
     this.tilemap = tilemap;
-}
-
-Pacman.Ghost.prototype.route = function(sprite, tile) {
-    debugger;
-}
+};
--- games/pacman/src/Helpers.js
+++ games/pacman/src/Helpers.js
@@ -1,14 +1,64 @@
 'use strict';
 Pacman.Helpers = {
     Tilemap: {
-        getSurroundingTiles: function(tilemap, x, y, layer) {
+        /**
+         * Gets tiles surrounding the given tile-coordinates
+         *
+         * @method Pacman.Helpers.Tilemap.getSurroundingTiles
+         * @param {number|string|Phaser.TilemapLayer} [layer] - The layer to get the tile from.
+         * @param {number} x - The x coordinate to get the tile from. In tiles, not pixels.
+         * @param {number} y - The y coordinate to get the tile from. In tiles, not pixels.
+         * @return {object} All surrounding tiles.
+         */
+        getSurroundingTiles: function(layer, x, y) {
+            var tilemap = layer.map;
             var layerIndex = tilemap.getLayer(layer);
+            
             return {
                 up: tilemap.getTileAbove(layerIndex, x, y),
                 down: tilemap.getTileBelow(layerIndex, x, y),
                 right: tilemap.getTileRight(layerIndex, x, y),
                 left: tilemap.getTileLeft(layerIndex, x, y)
             };
+        },
+
+        getValidDirections: function(direction, passableTiles, surroundingTiles) {
+            return {
+                canMoveUp: direction !== Phaser.DOWN && surroundingTiles.up && passableTiles.indexOf(surroundingTiles.up.index) > -1,
+                canMoveDown: direction !== Phaser.UP && surroundingTiles.down && passableTiles.indexOf(surroundingTiles.down.index) > -1,
+                canMoveLeft: direction !== Phaser.RIGHT && surroundingTiles.left && passableTiles.indexOf(surroundingTiles.left.index) > -1,
+                canMoveRight: direction !== Phaser.LEFT && surroundingTiles.right && passableTiles.indexOf(surroundingTiles.right.index) > -1
+            };
+        },
+        
+        getValidTilesToMoveTo: function(validDirections, surroundingTiles) {
+            var validTiles = []
+                if (validDirections.canMoveUp) {
+                    validTiles.push({
+                        tile: surroundingTiles.up,
+                        direction: Phaser.UP
+                    });
+                }
+                if (validDirections.canMoveDown) {
+                    validTiles.push({
+                        tile: surroundingTiles.down,
+                        direction: Phaser.DOWN
+                    });
+                }
+                if (validDirections.canMoveLeft) {
+                    validTiles.push({
+                        tile: surroundingTiles.left,
+                        direction: Phaser.LEFT
+                    });
+                }
+                if (validDirections.canMoveRight) {
+                    validTiles.push({
+                        tile: surroundingTiles.right,
+                        direction: Phaser.RIGHT
+                    });
+                }
+                
+                return validTiles.length > 0 ? validTiles : null;
         }
     }
 };
